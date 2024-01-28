1cf99836d2767d1889995111dfcf0a77f4b8475c
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-01-27 22:19:01 -0300

    wip: add userDataDir in server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'], // Desabilita o sandbox
-    headless: true 
+    headless: false ,
+    userDataDir: '/home/jorge/puppeteer_data' //@todo colocar em .env
 });
   const page = await browser.newPage();
   await page.goto('http://example.com');
