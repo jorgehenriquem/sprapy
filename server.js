ca760f22095b7b8e7a784efe0eabc4558775b8f3
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-01-27 22:11:05 -0300

    wip: adding puppeteer example working

---
+++ server.js
@@ -0,0 +1,14 @@
+
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], // Desabilita o sandbox
+    headless: true 
+});
+  const page = await browser.newPage();
+  await page.goto('http://example.com');
+  await page.screenshot({path: 'example.png'});
+  await browser.close();
+})();
